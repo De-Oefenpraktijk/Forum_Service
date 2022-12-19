e6938adfc22d35409b58979f470007bc3b1ff28e
raycohaex <raycoh1299@gmail.com>
2022-12-19 14:17:33 +0100

    added migrations

---
+++ Forum.Infrastructure/Migrations/20221219131655_username_added.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forum.Infrastructure.Migrations
+{
+    public partial class username_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "ForumPosts",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Comments",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "ForumPosts");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Comments");
+        }
+    }
+}
